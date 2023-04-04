f885513758cc551cc8aedc7c7cabad49165348cc
Hudson <hudsonbirdsong@gmail.com>
2023-04-04 16:39:53 -0500

    Build Error fix: removed import looking for favicon icon previously deleted

--- components/Works.jsx
+++ components/Works.jsx
@@ -2,7 +2,6 @@ import Tilt from 'react-parallax-tilt';
 import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { github } from '../assets';
-import vercelLogo from '../public/favicon.ico'
 import { SectionWrapper } from '../hoc';
 import { projects } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
